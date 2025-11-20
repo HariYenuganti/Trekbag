3885e97936e42a47d7f6dff5420886498aec9fda
HariYenuganti <haripranay07@gmail.com>
2025-11-19 20:25:20 -0500

    feat: add base UI components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      <small>© 2026 Copyright by Hari Yenuganti</small>
+      <p>
+        Version <b>1.5</b>
+      </p>
+    </footer>
+  );
+}
